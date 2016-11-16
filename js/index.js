f5c3c15d10bc907298aa36af7e97575a48505b5e
BuzznBias <nnamdiwill@gmail.com>
2016-11-16 02:56:29 +0000

    API Key added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 require('babel-polyfill');
+require('superagent');
+
+request
+   .post('https://api.edmunds.com/api/vehicle/v2/makes?fmt=json&api_key=kvytjhenbcuxked2g4r89zm9')
+   .send({ make: 'Chevy', model: 'camaro' })
+   .set('API-KEY', 'kvytjhenbcuxked2g4r89zm9')
+   .set('Car', 'application/json')
+   .end(function(err, res){
+     if (err || !res.ok) {
+       alert('Oh no! error');
+     } else {
+       alert('yay got ' + JSON.stringify(res.body));
+     }
+   });
 
